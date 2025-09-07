63b8cca26d87ef22ebbc8b39e64c1907058116a2
Md Abu Bakkar Siddik Hamim <programmerhamim@gmail.com>
2025-09-07 11:26:00 +0600

    Update bd_news.py

--- bd_news.py
+++ bd_news.py
@@ -12,7 +12,7 @@ client = LLMLayerClient(
 )
 
 response = client.search(
-    query="search top 10 today news about bangladesh.",
+    query="search top 5 today news about bangladesh with description",
     model="groq/llama-4-maverick-17b-128e-instruct",
     domain_filter=["dhakatribune.com", "thedailystar.net", "tbsnews.net",],
     return_sources= False,
